a35607d901f411cffcec3e94a29c9e96c00ab867
Ryder James <nickamilion@yahoo.com>
2018-11-20 15:23:24 -0700

    Reduce visibility and mutability of fields

--- src/edu/neumont/csc110/Player.java
+++ src/edu/neumont/csc110/Player.java
@@ -6,41 +6,40 @@ import edu.neumont.csc110.equipment.Property;
 import edu.neumont.csc110.equipment_abstract.Building;
 
 public class Player {
-
-	String name;
-	Piece piece;
+	private final String name;
+	private final Piece piece;
 	ArrayList<Property> properties;
-	
+
 	int balance;
-	
+
 	public Player(String name, Piece piece) {
 		this(name, piece, 1500);
 	}
-	
+
 	public Player(String name, Piece piece, int initBalance) {
 		this.name = name;
 		this.piece = piece;
 		this.balance = initBalance;
 		properties = new ArrayList<>();
 	}
-	
+
 	public boolean isBankrupt() {
 		return false;
 	}
-	
+
 	public int roll() {
 		return 0;
 	}
-	
+
 	public void mortgage(Property toMortgage) {
-		
+
 	}
-	
+
 	public void buyBuilding(Property toBuyOn, Building toBuy, int numberOfBuildings) {
-		
+
 	}
-	
+
 	public void sellBuilding(Property toSellFrom, Building toSell, int numberOfBuildings) {
-		
+
 	}
 }
