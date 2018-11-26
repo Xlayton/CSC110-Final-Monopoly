aa1d638eed6a6ad54103347e6e2fd5840f94ddea
Ryder James <nickamilion@yahoo.com>
2018-11-26 08:39:42 -0700

    Add card types

---
+++ src/edu/neumont/csc110/game_pieces/card_types/JailBreakCard.java
@@ -0,0 +1,16 @@
+package edu.neumont.csc110.game_pieces.card_types;
+
+import edu.neumont.csc110.game_pieces_abstract.Card;
+
+public class JailBreakCard extends Card {
+
+	protected JailBreakCard(String name, String flavorText) {
+		super(name, flavorText);
+	}
+
+	@Override
+	public void getEffect() {
+		
+	}
+
+}
---
+++ src/edu/neumont/csc110/game_pieces/card_types/TravelCard.java
@@ -0,0 +1,16 @@
+package edu.neumont.csc110.game_pieces.card_types;
+
+import edu.neumont.csc110.game_pieces_abstract.Card;
+
+public class TravelCard extends Card {
+
+	protected TravelCard(String name, String flavorText) {
+		super(name, flavorText);
+	}
+
+	@Override
+	public void getEffect() {
+		
+	}
+
+}
