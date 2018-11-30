9a5295b5399d0790a5663f492b0515260491f25e
Clayton Schrumpf <weightequalsmasstimesgravity@gmail.com>
2018-11-30 14:01:02 -0700

    Fix Trade... again

--- src/edu/neumont/csc110/game_pieces_abstract/Trade.java
+++ src/edu/neumont/csc110/game_pieces_abstract/Trade.java
@@ -1,12 +1,6 @@
 package edu.neumont.csc110.game_pieces_abstract;
 
-import edu.neumont.csc110.Player;
-
 public abstract class Trade {
-	
-	public OwnableSquare[] getOwnedLocations(Player toGet) {
-		return toGet.getTitleDeeds();
-	}
 	
 	public abstract void startTrade();
 	
