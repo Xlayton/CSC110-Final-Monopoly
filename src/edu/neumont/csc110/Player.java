501f386e24c141f3694401af56b2ee58a2af7556
Ryder James <nickamilion@yahoo.com>
2018-11-30 10:37:58 -0700

    Remove unused static field in Player

--- src/edu/neumont/csc110/Player.java
+++ src/edu/neumont/csc110/Player.java
@@ -7,7 +7,6 @@ import edu.neumont.csc110.game_pieces.TitleDeed;
 import edu.neumont.csc110.game_pieces_abstract.OwnableSquare;
 
 public class Player {
-	public static Player player;
 	private final String name;
 	private final Piece piece;
 	private final ArrayList<OwnableSquare> properties;
